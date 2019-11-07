0e384e5ff793eced0fecf03e45cd003fd2e7109e
Rocco Panacci <n01340339@nj-00118-001.humber.org>
2019-11-07 14:25:51 -0500

    Add the lab

--- 10/114/b/scheduler/js/index.js
+++ 10/114/b/scheduler/js/index.js
@@ -23,3 +23,26 @@ document.getElementById(`instructor`).innerHTML = course.instructor;
 document.getElementById(`start`).innerHTML = course.start;
 document.getElementById(`weeks`).innerHTML = course.weeks;
 document.getElementById(`duration`).innerHTML = course.duration;
+
+/*
+LAB:
+1. Fix the output of the start term by turning the Object's properties into a String
+      Ex: { term: `Fall`, year: 2019 }  >>  `Fall 2019`
+2. Write a function that will take a Number of minutes and will return a human-readable String
+      Ex: 160  >>  `2 hr, 40 min`
+      Hints:
+        - Division: /
+        - Remainder: %
+        - Rounding:
+            - Standard rules:     Math.round(4.5)
+            - Always round up:    Math.ceil(1.001)
+            - Always round down:  Math.floor(1.999)
+*/
+
+function getDurationFromMinutes(minutes) {
+
+
+  return `H hr, M min`;
+}
+
+getDurationFromMinutes(160);  // 2 hr, 40 min
