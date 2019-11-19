a343195a560b38f10df7a274ebd2ffd14517f582
Rocco Panacci <n01340339@nj-00118-001.humber.org>
2019-11-19 18:34:31 -0500

    Updated at half

---
+++ 12/113/b/array-methods/js/index.js
@@ -0,0 +1,49 @@
+// Countries that I have visited
+const countries = [
+  'Canada', 
+  'Japan',
+  'Mexico',
+  'Haiti',
+  'France'
+];
+
+
+function addToCountries(country) {
+  countries.push(country);
+  renderCountries();
+}
+
+function renderCountries() {
+  document.getElementById('visited').innerHTML = countries
+    .map(c => `<li>I have been to ${c}</li>`)
+    .join('\n');
+}
+
+// Run this the first time.
+renderCountries()
+
+
+
+
+
+
+// function getCountyAsHtml(country) {
+//   return `<li>I have been to ${country}</li>`;
+// }
+
+// const getCountyAsHtml = function(country) {
+//   return `<li>I have been to ${country}</li>`;
+// }
+
+// const getCountyAsHtml = (country) => {
+//   return `<li>I have been to ${country}</li>`;
+// }
+
+
+// let strOfCountries = '';
+// strOfCountries += getCountyAsHtml(countries[0]);
+// strOfCountries += getCountyAsHtml(countries[1]);
+// strOfCountries += getCountyAsHtml(countries[2]);
+// strOfCountries += getCountyAsHtml(countries[3]);
+// strOfCountries += getCountyAsHtml(countries[4]);
+// document.getElementById('visited').innerHTML = strOfCountries;
