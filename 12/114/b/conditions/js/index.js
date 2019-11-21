c873212358ce7e4b312b282f5f31a815ae3e90ca
Rocco Panacci <n01340339@nj-00118-001.humber.org>
2019-11-21 12:27:04 -0500

    updated after morning class.

---
+++ 12/114/b/conditions/js/index.js
@@ -0,0 +1,60 @@
+// BOOLEAN
+let classHasStarted = false;
+
+
+
+// CONDITIONS (IF-ELSE)
+
+
+
+// OPERATORS
+/* 
+- !
+- > >= < <= 
+- == != 
+- === !==
+*/
+
+
+
+// FALSY
+
+
+
+// IF-ELSE-ELSEIF (Control Statements)
+
+
+
+// PRACTICE 1:
+// Write a condition to product the following result
+// 20+:         It's hot
+// 5 -> 19:     It's warm
+// -4 -> 4:     It's comfortableå
+// -5 -> -19:   It's cold
+// -20:         It's freeeeeeeeezing
+// (Assume integers only)
+
+
+
+// LOGICAL GATES: OR/AND
+/* 
+AND &&
+OR  ||
+*/
+
+
+
+// PRACTICE 2:
+// Solve Practice 1 using logical gates
+
+
+
+// VARIABLE SCOPE
+
+
+
+// TERNARY
+
+
+
+// FILTER() ARRAYS
---
+++ 12/114/b/scheduler/js/index.js
@@ -0,0 +1,110 @@
+/******* GLOABL VARIABLES/DATA ********/
+// Define all of your variables here, including Object and Array references
+
+const allCourses = [
+  { // 0
+    name: `Tools and Workflow`,
+    code: `WDDM-115`,
+    instructor: `Kadeem Best`,
+    start: { term: `Fall`, year: 2019 },
+    weeks: 15,
+    breaks: true,
+    duration: 160
+  },{  // 1
+    name: `Applied Web Development`,
+    code: `WDDM-113`,
+    instructor: `Rocco Panacci`,
+    start: { term: `Winter`, year: 2020 },
+    weeks: 15,
+    breaks: true,
+    duration: 160
+  },{ // 2
+    name: `Applied Web Design`,
+    code: `WDDM-114`,
+    instructor: `Rocco Panacci`,
+    start: { term: `Fall`, year: 2019 },
+    weeks: 15,
+    breaks: true,
+    duration: 160
+  },{ // 3
+    name: `Design Technique`,
+    code: `WDDM-116`,
+    instructor: `Milorad Eftoski`,
+    start: { term: `Fall`, year: 2019 },
+    weeks: 15,
+    breaks: true,
+    duration: 160
+  },{ // 4
+    name: `Prototyping`,
+    code: `WDDM-117`,
+    instructor: `Cory Coletta`,
+    start: { term: `Fall`, year: 2019 },
+    weeks: 15,
+    breaks: true,
+    duration: 160
+  }
+];
+
+
+/************* FUNCTIONS *************/
+// Now store all the functions that will manipulate the data
+
+function getDurationFromMinutes(minutes) {
+  
+  const hrs = Math.floor(minutes / 60);
+  const mins = minutes % 60;
+  // const mins = minutes - hrs * 60; // Could also use this
+
+  return `${hrs} hr, ${mins} min`;
+}
+
+// Function:
+// Parameters: course:Object
+// Return: String of HTML (<article>)
+function getCourseAsHtmlString(course) {
+  return `
+    <article class="course">
+      <h3 id="name">${course.name}</h3>
+      <ul>
+        <li>Course Code: <strong>${course.code}</strong></li>
+        <li>Instructor: <strong>${course.instructor}</strong></li>
+        <li>Start: <strong>${course.start.term} ${course.start.year}</strong></li>
+        <li>
+          Weeks: <strong>${course.weeks}</strong>
+          <ul>
+            <li>Includes Break: <strong>${course.breaks}</strong></li>
+          </ul>
+        </li>
+        <li>Duration: <strong>${getDurationFromMinutes(course.duration)}</strong></li>
+      </ul>
+    </article>`;
+}
+
+/************* EXECUTABLE *************/
+
+
+window.addEventListener('load', () => {
+
+  // LAB:
+
+  // 1. Write a function that:
+  //    - receives a course:Object as a parameter
+  //    - returns true when the course starts 'Fall 2019'
+  //    - returns false in all other cases
+
+  // 2. Apply the function to `filter()` as a parameter
+  //    so that the list of 3 will only consist of courses
+  //    for Fall 2019
+
+
+
+  const printThreeCourses = (arr) => {
+    document.getElementById('courses').innerHTML = arr.slice(0, 3).map(getCourseAsHtmlString).join('\n')
+  }
+  
+  document.getElementById('loadCourses').addEventListener('click', () => {
+    printThreeCourses(allCourses)
+  });
+  
+});
+
