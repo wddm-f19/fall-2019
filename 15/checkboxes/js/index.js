166cf61e2550696dd3a401f87c7949333a83dbe1
Rocco Panacci <n01340339@nj-00118-001.humber.org>
2019-12-10 20:48:56 -0500

    review examples

--- 15/checkboxes/js/index.js
+++ 15/checkboxes/js/index.js
@@ -5,21 +5,33 @@ const tasks = [
   },{ 
     name: 'Complete homework',
     categories: ['home', 'school'] 
+  },{ 
+    name: 'Submit hours',
+    categories: ['work']
   }
 ];
 
 document.querySelectorAll('[name="tags"]').forEach(tag => {
   tag.addEventListener('change', event => {
-    console.log(event.target.value);
+    // console.log(event.target.value);
+    renderAllTasks(tasks);
   })
 });
 
 
-const getTaskAsHtml = (t) => {
-  return `<li>${t.name}</li>`;
+const getCategoryAsHtml = (c) => {
+  return `<span class="category ${c}">${c}</span>`
 }
 
+const getTaskAsHtml = (t) => {
+  return `
+    <li>
+      ${t.name} 
+      ${t.categories.map(getCategoryAsHtml).join('\n')}
+    </li>`;
+}
 
+// One time call
 const renderAllTasks = (arr) => {
 
   const whichTagsAreChecked = [];
@@ -30,8 +42,40 @@ const renderAllTasks = (arr) => {
     }
   });
 
-  console.log(whichTagsAreChecked);
+  // console.log('Only showing: ' + whichTagsAreChecked.join(', '));
+  // console.log(arr.map(t => t.categories.join(', ')));
 
+
+  arr = arr.filter(task => {
+
+    let isPartOfFilter = false;
+    whichTagsAreChecked.forEach(cat => {
+      if (task.categories.includes(cat))
+        isPartOfFilter = true;
+    });
+
+    console.log(task.name, isPartOfFilter);
+
+    if (isPartOfFilter) {
+      return true;
+    }
+    return false;
+  })
+
+
+
+  
+
+  /*
+  If a arr Object (a task) has any of the whichTagsAreChecked 
+  categories as part of its Array of categories, we should
+  filter-in(true), otherwise, filter-out(false)
+  */
+
+
+
+
+  // arr = arr.filter(task => task.categories.filter(cat => whichTagsAreChecked.includes(cat)));
   document.getElementById('allTasks').innerHTML = arr.map(getTaskAsHtml).join('')
 }
 
